a34d3f8451d000f1ca134cf25efab60939039e25
Valeria Estefania <valeriaberrio0506@gmail.com>
2026-03-13 11:49:45 -0500

    Fixing merge conflict

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-<<<<<<< ian
 
 app = FastAPI()
 
@@ -36,27 +35,6 @@ def delete_clientes(email: str):
             clients.remove(cliente)
             return "Se ha borrado existosamente"
     return "No se ha encontrado el cliente"
-=======
-
-app = FastAPI()
-
-clients = []
-
-@app.post("/addClient", tags=["Cliente"])
-def add_client(id: int, name: str, phone: str, email: str):
-    client = {
-        "id": id,
-        "name": name,
-        "phone": phone,
-        "email": email
-    }
-    clients.append(client)
-    return {"message": "Cliente agregado correctamente!", "client": client}
-
-@app.get("/getClients", tags=["Cliente"])
-def get_clients():
-    return {"Clientes": clients}
-
 
 Proteinas = []
 
@@ -76,4 +54,3 @@ def crearProteinas(Proteina:Proteina):
 def get_Proteina():
     return {"Proteina" : Proteina}
 
->>>>>>> main

e0256f6eeb9faf936e59c145767b4ff3e09bb68a
Ian Novoa <iannovoacc8@gmail.com>
2026-03-13 11:40:26 -0500

    CRUD cliente

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,37 @@
-import fastapi
+from fastapi import FastAPI
 
-app = fastapi.FastAPI()
+app = FastAPI()
+
+clients = []
+
+@app.post("/addClient", tags=["Cliente"])
+def add_client(name: str, lastname: str, address:str, phone: str, email: str):
+    client = {
+        "name": name,
+        "lasname":lastname,
+        "email": email,
+        "address": address,
+        "phone": phone,
+    }
+    clients.append(client)
+    return {"message": "Cliente agregado correctamente!", "client": client}
+
+@app.get("/getClients", tags=["Cliente"])
+def get_clients():
+    return {"Clientes": clients}
+
+@app.put("/putCliente", tags=["Cliente"])
+def put_cliente(email: str, cliente_update: dict):
+    for cliente in clients:
+        if cliente.get("email") == email:
+            cliente.update(cliente_update)
+            return "Se ha actualizado los datos del cliente correctamente"
+    return "No se ha encontrado el cliente con el email ingresado"
+
+@app.delete("/deleteCliente", tags=["Cliente"])
+def delete_clientes(email: str):
+    for cliente in clients:
+        if cliente.get("email") == email:
+            clients.remove(cliente)
+            return "Se ha borrado existosamente"
+    return "No se ha encontrado el cliente"

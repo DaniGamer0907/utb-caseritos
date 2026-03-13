2b5cae14c949096383422aaebce63c8fc0b4e863
DaniGamer0907 <danielmh0907@gmail.com>
2026-03-13 10:00:08 -0500

    Agregar a la api: addClient y getClients

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,20 @@
-import fastapi
+from fastapi import FastAPI
 
-app = fastapi.FastAPI()
+app = FastAPI()
+
+client = []
+
+@app.post("/addUser", tags=["Cliente"])
+def add_user(id: int, name: str, phone: str, email: str):
+    user = {
+        "id": id,
+        "name": name,
+        "phone": phone,
+        "email": email
+    }
+    client.append(user)
+    return {"message": "User added successfully", "user": user}
+
+@app.get("/getUsers", tags=["Cliente"])
+def get_users():
+    return {"users": client}

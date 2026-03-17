ef3196aa29591996ccb1fafe9324a05b50d67cc0
DaniGamer0907 <danielmh0907@gmail.com>
2026-03-17 05:52:03 -0500

    Conectando CRUD con base de datos

--- backend/modelos.py
+++ backend/modelos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String, Float
 from sqlalchemy.orm import declarative_base
 from db import engine
 
@@ -6,8 +6,23 @@ Base = declarative_base()
 
 class Proteina(Base):
     __tablename__ = "proteina"
-    Protein_id = Column(Integer,primary_key=True)
+    id = Column(Integer,primary_key=True)
     nombre = Column(String)
     avaliable = Column(Integer)
+
+class Cliente(Base):
+    __tablename__ = "cliente"
+    id = Column(Integer,primary_key=True)
+    nombre = Column(String, index=True)
+    lastname = Column(String)
+    address = Column(String)
+    phone = Column(String)
+    email = Column(String)
+
+class Almuerzo(Base):
+    __tablename__ = "tipoalmuerzo"
+    id = Column(Integer,primary_key=True)
+    nombre = Column(String)
+    precio = Column(Float)
 
 Base.metadata.create_all(engine)

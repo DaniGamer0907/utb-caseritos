25e5294d5e8c6a114adb66711ad2b44dc85de90b
DaniGamer0907 <danielmh0907@gmail.com>
2026-03-18 22:59:27 -0500

    Arreglando cositas

--- backend/routes/DetallePedido_routes.py
+++ backend/routes/DetallePedido_routes.py
@@ -4,46 +4,46 @@ from db import get_db
 from models.DetallePedido import DetallePedido
 from schemas.DetallePedido_schemas import DetallePedidoC
 
-router= APIRouter(prefix="/Detallepedido", tags= ["DetallePedido"])
+router= APIRouter(prefix="/detallesPedido", tags= ["Detalles de Pedidos"])
 
-@router.post("/crearDetallePedidos")
-def crear_detalle_pedido(detalle_pedido: DetallePedidoC, db: Session = Depends(get_db)):
+@router.post("/crearDetallesPedido")
+def crear_detalle_de_pedido(detalle_pedido: DetallePedidoC, db: Session = Depends(get_db)):
     pedidodb = DetallePedido(cantidad=detalle_pedido.cantidad, precio_unitario=detalle_pedido.precio_unitario, total=detalle_pedido.total )
     db.add(pedidodb)
     db.commit()
     return {"mensaje": "Detalle de Pedido agregado correctamente"}
 
-@router.get("/DetallePedidos")
-def obtener_detalle_pedido(db: Session = Depends(get_db)):
+@router.get("/listDetallesPedidos")
+def obtener_detalles_de_pedidos(db: Session = Depends(get_db)):
     pedidos = db.query(DetallePedido).all()
     if not pedidos:
         raise HTTPException(status_code=404, detail="Detalle de Pedidos no encontrados")
     else:
         return pedidos
 
-@router.get("/DetallePedidosID")
-def obtener_detalle_pedido_id(id: int, db: Session=Depends(get_db)):
+@router.get("/getDetallesPedido")
+def obtener_detalle_de_pedido(id: int, db: Session=Depends(get_db)):
     pedidos = db.query(DetallePedido).filter(DetallePedido.id==id).first()
     if not pedidos:
         raise HTTPException(status_code=404, detail="Detalle de Pedido no encontrado")
     else:
         return pedidos
     
-@router.put("/DetallePedidosID")
-def actualizar_detalle_pedido(id: int, nuevo_detalle_pedido: DetallePedidoC, db: Session = Depends(get_db)):
+@router.put("/actualizarDetallesPedido")
+def actualizar_detalle_de_pedido(id: int, nuevo_detalle_pedido: DetallePedidoC, db: Session = Depends(get_db)):
     pedidos = db.query(DetallePedido).filter(DetallePedido.id==id).first()
     if not pedidos:
         raise HTTPException(status_code=404, detail="Detalle de Pedido no encontrado")
     else:
-        pedidos.cantidad = nuevo_detalle_pedido.cantidad
-        pedidos.precio_unitario = nuevo_detalle_pedido.precio_unitario
-        pedidos.total = nuevo_detalle_pedido.total
+        pedidos.cantidad = nuevo_detalle_pedido.cantidad #type: ignore
+        pedidos.precio_unitario = nuevo_detalle_pedido.precio_unitario #type: ignore
+        pedidos.total = nuevo_detalle_pedido.total #type: ignore
         db.commit()
         return {"mensaje": "Detalle de Pedido actualizado correctamente"}
     
 
-@router.delete("/DetallePedidosID")
-def eliminar_detalle_pedido(id: int, db: Session = Depends(get_db)):
+@router.delete("/borrarDetallesPedido")
+def eliminar_detalle_de_pedido(id: int, db: Session = Depends(get_db)):
     pedidos = db.query(DetallePedido).filter(DetallePedido.id==id).first()
     if not pedidos:
         raise HTTPException(status_code=404, detail="Detalle de Pedido no encontrado")

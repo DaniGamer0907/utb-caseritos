538e44bc040df7ecacbba545e45835a40a389ad4
DaniGamer0907 <danielmh0907@gmail.com>
2026-04-13 00:21:05 -0500

    Agregando componentes de home y de login con rutas, agregando estilos y plantillas.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterModule],
+  template: `<router-outlet></router-outlet>`,
   styleUrl: './app.css'
 })
 export class App {

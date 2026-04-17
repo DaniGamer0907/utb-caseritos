93d7543f5099a5afac27f953a33a4684858359d9
DaniGamer0907 <danielmh0907@gmail.com>
2026-04-17 03:33:13 -0500

    Conectando registrar con la API

--- frontend/src/app/registrar/registrar.ts
+++ frontend/src/app/registrar/registrar.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: 'registrar.html',
-  styleUrl: './registrar.css',
+  styleUrls: ['./registrar.css'],
 })
-export class Registrar {}
+export class Registrar {
+  usuario = {
+    name: '',
+    lastname: '',
+    password: '',
+    address: '',
+    phone: '',
+    email: ''
+  };
+  authService: AuthService = inject(AuthService);
+  onRegistro(){
+    console.log("hola1")
+    this.authService.registro(this.usuario).subscribe({
+      next: (res: any) => {
+        alert(res.message); // "Usuario registrado correctamente"
+        // Aquí podrías redirigir al login
+      },
+      error: (err) => {
+        alert('Error: ' + err.error.detail);
+      }
+    });
+    console.log("hola2")
+  }
+}

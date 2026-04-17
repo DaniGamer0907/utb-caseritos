608fd600bcd8a0d14758d9ae0048135ddf894495
DaniGamer0907 <danielmh0907@gmail.com>
2026-04-17 03:33:43 -0500

    Conectando login con la API

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../auth-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  authService: AuthService = inject(AuthService);
+  router: Router = inject(Router);
+  email = '';
+  password = '';
+  onLogin(){
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.access_token);
+        localStorage.setItem('role', res.role);
+        console.log('¡Adentro!', res.role);
+        this.router.navigate(['/home'])
+      },
+      error: (err) => alert('Error: ' + err.error.detail)
+    });
+  }
+}
